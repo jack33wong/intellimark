1076eebfde50c3f75c4c536fe054cf38839a05ea
imacpro <jack.33.wong@gmail.com>
2025-09-08 17:08:30 +0100

    chore: remove deploy folder from git tracking
    - Remove deploy/ folder from git tracking while keeping files locally
    - Deploy folder is now properly ignored by .gitignore
    - Prevents build artifacts from being tracked in version control

--- deploy/functions/index.js
+++
@@ -1,5 +0,0 @@
-const functions = require('firebase-functions');
-const app = require('./server.js');
-
-// Export the Express app as a Firebase Function
-exports.api = functions.https.onRequest(app);
